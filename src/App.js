a820ee5bd16e743620dfb0ddacd4e55b27670a19
sandibansal <thisissandibansal@gmail.com>
2018-11-26 17:14:55 +0530

    Person functional component :mega: :mega:

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Person from './Person/Person';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hi, I am react app</h1>
+        <p>This is really working!!!</p>
+        <Person />
       </div>
     );
   }
---
+++ src/Person/Person.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Person = () => {
+  return <p>I' am a person!</p>;
+};
+
+export default Person;
