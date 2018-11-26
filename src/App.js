83689b1a53b053dc5f066f05778115189f912ebf
sandibansal <thisissandibansal@gmail.com>
2018-11-26 22:20:50 +0530

    Setting style dynamically :pizza: :pizza:

--- src/App.js
+++ src/App.js
@@ -71,10 +71,19 @@ class App extends Component {
       style.backgroundColor = 'red';
     }
 
+    // Setting classes dynamically
+    let classes = [];
+    if (this.state.persons.length <= 2) {
+      classes.push('red');
+    }
+    if (this.state.persons.length <= 1) {
+      classes.push('bold');
+    }
+
     return (
       <div className="App">
         <h1>Hi, I am react app</h1>
-        <p>This is really working!!!</p>
+        <p className={classes.join(' ')}> This is really working!!!</p>
         <button style={style} onClick={this.togglePersonHandler}>
           Toggle Persons
         </button>
